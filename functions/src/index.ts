74718c536d84c5584eedd2e0c20ff4459a7a1f08
y129may9th <y129may9th@gmail.com>
2023-03-16 19:00:03 +0900

    feat: add create user function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,11 @@ import * as functions from "firebase-functions";
 import user = require("./user");
 
 export const execute = functions.https.onRequest(async (request, response) => {
-  const result = await user.selectUser(request.body);
+  /** insert */
+  const result = await user.createUser(request.body);
+
+  /** select */
+  // const result = await user.selectUser(request.body);
+
   response.send(result.record);
 });
--- functions/src/table/users.ts
+++ functions/src/table/users.ts
@@ -5,6 +5,46 @@ import dotenv = require("dotenv");
 dotenv.config();
 
 // =======================================
+// 作成
+// =======================================
+export async function create(params: object) {
+  // MySQLへ接続
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+
+  const columns: string[] = [];
+  const data: any[] = [];
+
+  Object.entries(params).forEach(([key, value]) => {
+    columns.push(key);
+    data.push(value);
+  });
+
+  try {
+    const results = await connection.query(
+        "INSERT INTO users (??) VALUES (?)",
+        [columns, data]
+    );
+    return results;
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      // Error型であることを確認したらエラーメッセージを投げる。
+      throw new functions.https.HttpsError("internal", e.message);
+    } else {
+      // Error型でなかった場合も何らかthrowを書かないとエラーになる
+      throw console.log("other issues");
+    }
+  } finally {
+    await connection.end();
+  }
+}
+
+// =======================================
 // 取得
 // =======================================
 export async function select(params: { id: number }) {
